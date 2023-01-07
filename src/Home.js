b7cc3e2d2c7f4408fea51b945395df9cd2678a39
Hinal05 <hinal.parikh@qed42.com>
2023-01-07 14:07:06 +0530

    main: Install prompt added.

--- src/Home.js
+++ src/Home.js
@@ -1,78 +1,85 @@
 import React, {useEffect, useState} from 'react';
 import {Button} from 'react-bootstrap';
+import iconIOS from "./images/upload.png";
 
-var deferredPrompt;
 export default function Home() {
-  const [showInstallMessage, setShowInstallMessage] = useState(false);
+  const [isIOS, setIsIOS] = useState(false);
 
   useEffect(() => {
-    // var temp = navigator.userAgent.match(/chrome|chromium|crios|safari|iphone|ipad|ipod/i);
-    // if (temp === undefined) {
-    //   setCheckBrowser(false);
-    // } else if (temp[0] === "Chrome" || temp[0] === "Safari") {
-    //   setCheckBrowser(true);
-    // }
-    window.addEventListener("beforeinstallprompt", (e) => {
-      // Prevent the mini-infobar from appearing on mobile
-      e.preventDefault();
-
-      // Stash the event so it can be triggered later.
-      deferredPrompt = e;
-      // Update UI notify the user they can install the PWA
-      // setInstallable(true);
+    // For Android.
+    let deferredPrompt; // Variable should be out of scope of addEventListener method
+    window.addEventListener('beforeinstallprompt', (e) => {
+        e.preventDefault();	// This prevents default chrome prompt from appearing
+        deferredPrompt = e;	 // Save for later
+        let infoBar = document.querySelector('#custom-info-bar');
+        if (infoBar) {
+          infoBar.style.display = '';
+          let installBtn = document.querySelector('#custom-install-button');
+          installBtn.addEventListener('click', (e) => {
+            //This prompt window
+            deferredPrompt.prompt();
+            // Here we wait for user interaction
+            deferredPrompt.userChoice
+            .then((choiceResult) => {
+              console.log(choiceResult);
+              if (choiceResult.outcome === 'accepted') {
+                let infoBar = document.querySelector('#custom-info-bar');
+                infoBar.style.display = 'none'; // Hide info bar
+                deferredPrompt = null; // not need anymore
+                alert('Accepted Install prompt.');
+              }
+              if (choiceResult.outcome === 'dismissed') {
+                alert('Dismissed Install prompt.');
+              }
+            });
+          });
+        }
     });
 
-    window.addEventListener("appinstalled", () => {
-      // Log install to analytics
-      //     console.log("INSTALL: Success");
-    });
+    // For IOS.
+    // Detects if device is on iOS
+    const isIosDevice = () => {
+      const userAgent = window.navigator.userAgent.toLowerCase();
+      return /iphone|ipad|ipod|macintosh/.test(userAgent);
+    }
 
     // Detects if device is on iOS
-    const isIos = () => {
-      // const userAgent = window.navigator.userAgent.toLowerCase();
-      // return /iphone|ipad|ipod/.test( userAgent );
-      return [
-        'iPad Simulator',
-        'iPhone Simulator',
-        'iPod Simulator',
-        'iPad',
-        'iPhone',
-        'iPod'
-      ].includes(navigator.platform)
-      // iPad on iOS 13 detection
-      || (navigator.userAgent.includes("Mac") && "ontouchend" in document);
+    const isSafari = () => {
+      return navigator.vendor && navigator.vendor.indexOf('Apple') > -1 &&
+        navigator.userAgent &&
+        navigator.userAgent.indexOf('CriOS') === -1 &&
+        navigator.userAgent.indexOf('FxiOS') === -1;
     }
-    // Detects if device is in standalone mode
-    const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
 
-    // Checks if should display install popup notification:
-    if (isIos() && !isInStandaloneMode()) {
-      setShowInstallMessage({ showInstallMessage: true });
+    if (isIosDevice() && isSafari()) {
+      setIsIOS(true);
     }
-  }, []);
+  }, [setIsIOS, isIOS]);
 
-  const handleInstallClick1 = (e) => {
-    // Hide the app provided install promotion
-    // setInstallable(false);
-    if (deferredPrompt !== undefined) {
-      // Show the install prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-    } else {
-      deferredPrompt = null;
-    }
-  };
 
   return (
-    <div>
+    <div className='home-wrapper'>
       <h1>Home Page</h1>
-      {/* <div className="prompt">
-        <p>Do you want to add this site to your home screen?</p>
-        <button type="button" className="prompt__install">Yes Please</button>
-        <button type="button" className="prompt__close">No Thanks</button>
-      </div> */}
-      <Button variant="dark" onClick={handleInstallClick1}>Install</Button>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Nulla porttitor accumsan tincidunt. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus suscipit tortor eget felis porttitor volutpat. Donec sollicitudin molestie malesuada.</p>
 
+      <p>Vivamus suscipit tortor eget felis porttitor volutpat. Pellentesque in ipsum id orci porta dapibus. Vivamus suscipit tortor eget felis porttitor volutpat. Pellentesque in ipsum id orci porta dapibus. Nulla porttitor accumsan tincidunt. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>
+
+      <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Sed porttitor lectus nibh. Nulla porttitor accumsan tincidunt. Donec sollicitudin molestie malesuada. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Donec sollicitudin molestie malesuada.</p>
+
+      <p>Cras ultricies ligula sed magna dictum porta. Sed porttitor lectus nibh. Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Quisque velit nisi, pretium ut lacinia in, elementum id enim.</p>
+
+      <p>Nulla porttitor accumsan tincidunt. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Vivamus suscipit tortor eget felis porttitor volutpat. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula.</p>
+
+      { isIOS ? (
+        <div className='sticky-modal'>Install the app on your iPhone: tap <span style={{width: "16px", display: "inline-block"}}><img src={iconIOS} width="100%" alt="" /></span> and then Add to homescreen.</div>
+      ) : (
+        <>
+        <div className='sticky-modal'>Install the app on your iPhone: tap <span style={{width: "16px", display: "inline-block"}}><img src={iconIOS} width="100%" alt="" /></span> and then Add to homescreen.</div>
+        <div id='custom-info-bar'>
+          <Button variant="dark" id='custom-install-button'>Install</Button>
+        </div>
+        </>
+      )}
     </div>
   )
 }
